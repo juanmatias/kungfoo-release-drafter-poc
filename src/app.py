700fd594fe89e8f0f22fe5e04bdaf51f9e4ec592
Juan Matias Kungfoo de la Camara Beovide <juanmatias@gmail.com>
2022-09-01 10:57:54 -0300

    TEST3 major change

--- src/app.py
+++ src/app.py
@@ -6,15 +6,21 @@ from flask import Flask
 # current module (__name__) as argument.
 app = Flask(__name__)
 
+
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'not authorized'
+
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
-@app.route('/')
+@app.route('/_home')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
     return 'release drafter sample app'
 
-@app.route('/about')
+@app.route('/_about')
 # ‘/’ URL is bound with hello_world() function.
 def about_world():
     return 'this is the about'
